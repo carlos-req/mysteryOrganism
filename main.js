986cab644b7cef06b9d4154482921e16a5b964bf
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2022-08-20 23:41:28 -0400

    finalize project

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
+  return dnaBases[Math.floor(Math.random() * 4)]
 };
 
 // Returns a random single stand of DNA containing 15 bases
@@ -13,7 +13,72 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//Return new object of pAequor
+const pAequorFactory = (specimanNum, dna) =>{
+  return {
+    specimanNum: specimanNum,
+    dna: dna,
+    mutate() {
+      let baseFinder = Math.floor(Math.random() * 15);
+      switch (this.dna[baseFinder]){
+        case 'A':
+          this.dna[baseFinder] = 'T'
+          break;
+        case 'T':
+            this.dna[baseFinder] = 'C'
+            break;
+        case 'C':
+          this.dna[baseFinder] = 'G'
+          break;
+        case 'G':
+          this.dna[baseFinder] = 'A'
+          break;  
+      };
+    },
+    compareDNA(obj) {
+      let sameDnaCounter = 0;
+      for (let i= 0; i <this.dna.length; i++){
+        if (this.dna[i] === obj.dna[i]){
+          sameDnaCounter++
+        };
+      };
+      let percentSame = (sameDnaCounter / this.dna.length) * 100;
+      console.log(`Specimen #1 and Specimen #2 have ${percentSame.toFixed(2)}% DNA in common`)
 
+    },
+    willLikelySurvive() {
+      let surviveCounter = 0;
+      for(let i=0; i < this.dna.length; i++){
+        if (this.dna[i] == 'C' || this.dna[i] == 'G'){
+          surviveCounter++
+        };
+      };
+      let percentSurvival = surviveCounter/this.dna.length;
+      console.log(percentSurvival);
+      if (percentSurvival => .60){
+        return true;
+      }else {
+        return false; 
+      }
+    },
+  };
+
+};
+
+const thirtyInstances = () =>{
+  let thirtySurvivals = [];
+  let i =1;
+  while (thirtySurvivals.length < 30){
+  let instance = pAequorFactory(i, mockUpStrand);
+  if(instance.willLikelySurvive()){
+    thirtySurvivals.push(instance);
+    };
+  i++;
+  };
+  return thirtySurvivals;
+}
+
+console.log(thirtyInstances());
 
 
 
